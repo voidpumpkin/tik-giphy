8755faeb4f4d5917b50ae25aee9c304037bbd4ae
Julius Lungys <32368314+voidpumpkin@users.noreply.github.com>
2021-05-26 00:24:21 +0300

    add dotenv

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -1,16 +1,23 @@
 use actix_web::{middleware::Logger, App, HttpServer};
+use dotenv::dotenv;
+use std::env;
 
 mod resources;
+
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
+    dotenv().ok();
+
     env_logger::init_from_env(env_logger::Env::new().default_filter_or("info"));
+
+    let host: String = env::var("BACKEND_HOST").unwrap_or("127.0.0.1:8081".into());
 
     HttpServer::new(|| {
         App::new()
             .wrap(Logger::default())
             .service(resources::users())
     })
-    .bind("127.0.0.1:8081")?
+    .bind(host)?
     .run()
     .await
 }
