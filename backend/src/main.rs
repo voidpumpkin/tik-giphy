76bafd04ece7710598c590ebb3b48737aa245508
Julius Lungys <32368314+voidpumpkin@users.noreply.github.com>
2021-05-26 23:50:05 +0300

    remove dotenv

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -1,14 +1,11 @@
 use actix_web::{middleware::Logger, App, HttpServer};
-use dotenv::dotenv;
 use std::env;
 
 mod resources;
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-    dotenv().ok();
-
-    env_logger::init_from_env(env_logger::Env::new().default_filter_or("info"));
+    env_logger::init_from_env(env_logger::Env::new().default_filter_or("INFO"));
 
     let host: String = env::var("BACKEND_HOST").unwrap_or("127.0.0.1:8081".into());
 
